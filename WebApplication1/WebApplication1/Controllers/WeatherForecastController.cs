55f4bdade7a701d472280ab4bf619b8ac803325a
Rahul RahulKumar Tripathi <rahul.tripathi10@mpdit.onmicrosoft.com>
2026-08-28 21:32:13 +0530

    First Commit

--- WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
+++ WebApplication1/WebApplication1/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace TestApplication.API.Controllers
+namespace WebApplication1.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- WebApplication1/WebApplication1/WeatherForecast.cs
+++ WebApplication1/WebApplication1/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace TestApplication.API
+namespace WebApplication1
 {
     public class WeatherForecast
     {
